a9edfebba93babb3a0e2653cf3f8bb5fc84b86a1
Diego Rodrigo Samame Salazar <tsm_dsamame@niubiz.com.pe>
2024-03-12 17:26:47 -0500

    space probe visual code git workaraound

--- src/services/facts.js
+++ src/services/facts.js
@@ -3,7 +3,7 @@ const CAT_ENDPOINT_FACT = 'https://catfact.ninja/fact'
 
 export async function getRamdonFact() {
     const res = await fetch(CAT_ENDPOINT_FACT)
-    const data = await res.json()
+    const data = await res.json() 
     const { fact } = data
     return fact
 }
